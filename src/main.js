7d967903382de1b7841e9b156cc4b513fd39e784
Marina.S. <marina.sevostian@gmail.com>
2024-10-26 07:08:03 +0300

    add html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+import { fetchPicture } from './js/pixabay-api';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let gallery = document.querySelector('.gallery');
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
