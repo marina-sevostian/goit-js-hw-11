24c010c3ec549c1ca57c9f2bba548899d8741db6
marina-sevostian <140451228+marina-sevostian@users.noreply.github.com>
2024-10-28 00:17:59 +0000

    Deploying to gh-pages from @ marina-sevostian/goit-js-hw-11@4f2cd7abacd35e83419bea3e511b0a2571973e86 🚀

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import{S as c,i as u}from"./assets/vendor-BrddEoy-.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function r(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(t){if(t.ep)return;t.ep=!0;const o=r(t);fetch(t.href,o)}})();function f(s){return fetch(`https://pixabay.com/api/?key=46729875-7729b8e358007a47de817f6d1&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(e=>{if(!e.ok){if(e.status===404)return[];throw new Error(e.status)}return e.json()}).catch(e=>{console.error(e)})}let m=document.querySelector(".gallery");function d(s){const e=s.map(r=>`<div class="photo">
-   <a class="gallery-link" href="${r.largeImageURL}">
+import{S as c,i as u}from"./assets/vendor-BrddEoy-.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();function f(s){return fetch(`https://pixabay.com/api/?key=46729875-7729b8e358007a47de817f6d1&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(r=>{if(!r.ok){if(r.status===404)return[];throw new Error(r.status)}return r.json()}).catch(r=>{iziToast.error({title:"Error",message:"Error! Sorry, something went wrong. This is an error!",color:"#ef4040",close:!1})})}let m=document.querySelector(".gallery");function d(s){const r=s.map(t=>`<div class="photo">
+   <a class="gallery-link" href="${t.largeImageURL}">
     <img
       class="gallery-image"
-      src="${r.webformatURL}"
-      data-source="${r.largeImageURL}"
-      alt="${r.tags}"
+      src="${t.webformatURL}"
+      data-source="${t.largeImageURL}"
+      alt="${t.tags}"
     />
   </a>
   <div class="info">
-  <p class="info-item">Likes <span class="info-item-num">${r.likes}</span></p>
-  <p class="info-item">Views <span class="info-item-num">${r.views}</span></p>
-  <p class="info-item">Comments <span class="info-item-num">${r.comments}</span></p>
-  <p class="info-item">Downloads <span class="info-item-num">${r.downloads}</span></p>
+  <p class="info-item">Likes <span class="info-item-num">${t.likes}</span></p>
+  <p class="info-item">Views <span class="info-item-num">${t.views}</span></p>
+  <p class="info-item">Comments <span class="info-item-num">${t.comments}</span></p>
+  <p class="info-item">Downloads <span class="info-item-num">${t.downloads}</span></p>
   </div>
-     </div> `).join("");m.insertAdjacentHTML("beforeend",e)}let p=document.querySelector(".gallery");document.querySelector(".search-form-btn");let i=document.querySelector(".search-form"),h=document.querySelector(".search-form-input"),y=new c(".gallery a"),n=document.querySelector(".loader");n.classList.replace(".loader",".hide");i.addEventListener("submit",s=>{s.preventDefault();const e=h.value.trim();g(),n.classList.replace(".hide",".loader"),e!==""&&(f(e).then(r=>{r.hits.length===0&&u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",color:"#ef4040",close:!1}),d(r.hits),y.refresh(),n.classList.replace(".loader",".hide")}),i.reset())});new c(".gallery a",{captionsData:"alt",captionDelay:250});function g(){p.innerHTML=""}
+     </div> `).join("");m.insertAdjacentHTML("beforeend",r)}let p=document.querySelector(".gallery");document.querySelector(".search-form-btn");let l=document.querySelector(".search-form"),h=document.querySelector(".search-form-input"),y=new c(".gallery a"),n=document.querySelector(".loader");n.classList.replace("loader","hide");l.addEventListener("submit",s=>{s.preventDefault();const r=h.value.trim();g(),n.classList.replace("hide","loader"),r!==""&&(f(r).then(t=>{t.hits.length===0&&u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",color:"#ef4040",close:!1}),d(t.hits),y.refresh(),n.classList.replace("loader","hide")}),l.reset())});new c(".gallery a",{captionsData:"alt",captionDelay:250});function g(){p.innerHTML=""}
 //# sourceMappingURL=index.js.map
